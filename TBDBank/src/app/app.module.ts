20cdb34e8b98f3e26290febdbd73cf50d19e3674
SethEvry <92412142+SethEvry@users.noreply.github.com>
2022-12-16 13:08:56 -0500

    create footer component

--- TBDBank/src/app/app.module.ts
+++ TBDBank/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
