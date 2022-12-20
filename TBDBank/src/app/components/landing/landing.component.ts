a070fa456ff5656bc56ded46b5287f6eed13ec52
Elian <efelix.v@gmail.com>
2022-12-20 15:25:56 -0500

    Fix view format for form

--- TBDBank/src/app/components/landing/landing.component.ts
+++ TBDBank/src/app/components/landing/landing.component.ts
@@ -4,11 +4,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {
-
-  constructor(private router: Router){}
+  constructor(private router: Router) {}
 
   onClick() {
     this.router.navigate(['/register']);
