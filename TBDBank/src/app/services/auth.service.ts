35e319b2eddeb32076f546b45f432bb18fb752cc
Elian <efelix.v@gmail.com>
2022-12-26 15:07:15 -0500

    Add user authentication

--- TBDBank/src/app/services/auth.service.ts
+++ TBDBank/src/app/services/auth.service.ts
@@ -1,20 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserLogin } from '../models/UserLogin';
-
+import { environment as env } from '../environment/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  apiUrl = env.API_URL;
+
   constructor(private http: HttpClient) {}
 
   login(userLogin: UserLogin) {
-    return (
-      // this is just the HTTP call, still need to handle the reception of the token
-      this.http.post<UserLogin>('/api/login', userLogin)
-    );
+    // this is just the HTTP call, still need to handle the reception of the token
+    return this.http.post<UserLogin>(`${this.apiUrl}login`, userLogin);
+  }
+
+  private setJwtSession(authResponse: { idToken: string }) {
+    return localStorage.setItem('id_token', authResponse.idToken);
+  }
+
+  public isLoggedIn() {
+    if (localStorage.getItem('id_token')) {
+      return true;
+    }
+    return false;
   }
 
   // delete local jwt and return to home page
-  logout() {}
+  logout() {
+    localStorage.removeItem('id_token');
+    return !this.isLoggedIn();
+  }
 }
